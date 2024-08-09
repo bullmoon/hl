5e10791ed28f765ae9532364dfcd7c57af1cc949
VV <vlad.vysotskiy@gmail.com>
2024-08-09 18:31:51 +0300

    Pulses with TI

--- app/automotive/routes.py
+++ app/automotive/routes.py
@@ -15,7 +15,7 @@ def automotive_default():
 
 @automotive.route('/emission/<test>')
 def automotive_emission(test):
-    valid_tests = ["r10", "tl81000-535", "tl81000-536", "cispr25-63", "cispr25-64", "cispr25-65"]
+    valid_tests = ["r10", "tl81000-535", "tl81000-536", "cispr25-63", "cispr25-64", "cispr25-65", "7637-2-43"]
     if test in valid_tests:
         return render_template('automotive_emission_test.html', test=test)
     else:
@@ -23,7 +23,7 @@ def automotive_emission(test):
 
 @automotive.route('/immunity/<test>')
 def automotive_immunity(test):
-    valid_tests = ["11452-2", "11452-4", "11452-9", "7637-2", "tl81000-523", "tl81000-525"]
+    valid_tests = ["11452-2", "11452-4", "11452-9", "7637-2-44", "tl81000-523", "tl81000-525"]
     if test in valid_tests:
         return render_template('automotive_immunity_test.html', test=test)
     else:
